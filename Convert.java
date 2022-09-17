a61a92c403386ad0862bc74058717bde786cb9f3
Mushegi <93542704+MushegM@users.noreply.github.com>
2022-09-17 19:33:47 +0300

    Add files via upload

---
+++ Convert.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import java.util.TreeMap;
+
+public class Convert {
+    private final static TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+
+    static {
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+    }
+
+    public final static String toRoman(int number) {
+        int l =  map.floorKey(number);
+        if ( number == l ) {
+            return map.get(number);
+        }
+        return map.get(l) + toRoman(number-l);
+    }
+
+}
